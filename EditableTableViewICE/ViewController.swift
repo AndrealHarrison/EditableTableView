1c1fb9507bd9cf181fed53522616b9412e767709
Andrea Harrison <Andrealharrison@gmail.com>
2022-05-17 14:53:06 -0700

    working on ice

--- EditableTableViewICE/ViewController.swift
+++ EditableTableViewICE/ViewController.swift
@@ -24,3 +24,36 @@ class EditableViewController: UITableViewController {
     }
 }
 
+class Animal {
+    var animalName: String
+    var color: String
+    var type: String
+
+    init(animalName: String, color:String, type: String) {
+        self.animalName = animalName
+        self.color = color
+        self.type = type
+    
+    }
+
+    convenience init() {
+        self.init(animalName:"[Unnamed]", color:"[blank]", type: <#String#>)
+    }
+}
+
+class DataStore {
+    var persons:[Animal]
+    init() {
+        let animal1 = Animal(animalName: "Billy", color:"red and white", type:"dog")
+        let animal2 = Animal(animalName: "Leo", color:"grey and white", type:"cat")
+        let animal3 = Animal(animalName: "Lucy", color:"black", type:"cat")
+    persons = [animal1, animal2, animal3]
+    }
+
+}
+
+class newCustomCell: UITableViewCell {
+
+}
+
+
